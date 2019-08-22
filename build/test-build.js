3e077fb2a8e327b624922812fc93dfaa9f073f1a
liliang <13366698969@163.com>
2019-08-22 10:07:19 +0800

    打包

--- build/test-build.js
+++ build/test-build.js
@@ -12,25 +12,30 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.test.conf')
 
-const spinner = ora('building for test...')
+const spinner = ora('building for ' + process.env.NODE_ENV + ' of ' + process.env.EVN_CONFIG + ' test...')
 spinner.start()
-var assetsPath = path.join(config.test.assetsRoot, config.test.assetsSubDirectory);
-shell.rm('-rf', assetsPath);
-shell.mkdir('-p', assetsPath);
-shell.config.silent = true;
-shell.cp('-R', 'static/*', assetsPath);
-shell.config.silent = false;
-webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
+rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     if (err) throw err
-    process.stdout.write(stats.toString({
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false
-    }) + '\n\n');
-    console.log(chalk.cyan('  Build complete.\n'));
-    console.log(chalk.cyan('  开始上传到服务器.\n'));
-    shell.exec('gulp deploy');
-});
+    webpack(webpackConfig, (err, stats) => {
+        spinner.stop()
+        if (err) throw err
+        process.stdout.write(stats.toString({
+            colors: true,
+            modules: false,
+            children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+            chunks: false,
+            chunkModules: false
+        }) + '\n\n')
+        
+        if (stats.hasErrors()) {
+            console.log(chalk.red('  Build failed with errors.\n'))
+            process.exit(1)
+        }
+        
+        console.log(chalk.cyan('  Build complete.\n'))
+        console.log(chalk.yellow(
+            '  Tip: built files are meant to be served over an HTTP server.\n' +
+            '  Opening index.html over file:// won\'t work.\n'
+        ))
+    })
+})
